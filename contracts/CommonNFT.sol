6ff6a35865898ad765dcbc470284f787707f2341
zen-lenovo <zennomi.kun@gmail.com>
2024-04-15 14:01:12 +0700

    nft verse marketplace

---
+++ contracts/CommonNFT.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CommonNFT is ERC721, ERC721URIStorage, Ownable {
+    constructor(address initialOwner) ERC721("MyNFT", "MNFT") {}
+
+    uint256 private _nextTokenId;
+
+    function safeMint(address to, string memory uri) public {
+        _safeMint(to, _nextTokenId);
+        _setTokenURI(_nextTokenId, uri);
+        _nextTokenId++;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
